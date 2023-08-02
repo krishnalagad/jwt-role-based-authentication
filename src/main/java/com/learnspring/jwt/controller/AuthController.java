29edb215e3db01c9d308573b47add778e4cf22d0
Krishna Lagad <krishnalagad2@gmail.com>
2023-08-02 23:43:17 +0530

    - code working fine.

--- src/main/java/com/learnspring/jwt/controller/AuthController.java
+++ src/main/java/com/learnspring/jwt/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.learnspring.jwt.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @PostMapping("/")
     public ResponseEntity<JwtAuthResponse> login(@RequestBody LoginDto loginDto) {
 
         String token = this.authService.login(loginDto);
