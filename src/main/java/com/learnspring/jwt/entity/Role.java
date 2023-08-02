d39fd003dcbad9390ad9ddf11f2a2a0186e7038d
Krishna Lagad <krishnalagad2@gmail.com>
2023-08-02 18:56:24 +0530

    - key constraints configurations on entity classes.

--- src/main/java/com/learnspring/jwt/entity/Role.java
+++ src/main/java/com/learnspring/jwt/entity/Role.java
@@ -1,7 +1,6 @@
 package com.learnspring.jwt.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -13,6 +12,8 @@ import lombok.*;
 @ToString
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
--- src/main/java/com/learnspring/jwt/entity/User.java
+++ src/main/java/com/learnspring/jwt/entity/User.java
@@ -1,7 +1,6 @@
 package com.learnspring.jwt.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -13,10 +12,20 @@ import lombok.*;
 @ToString
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
     private String profile;
 }
