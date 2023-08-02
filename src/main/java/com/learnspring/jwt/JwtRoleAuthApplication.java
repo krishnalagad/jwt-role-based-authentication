3bc8db19d614676f204d3e47732c7728ce4b76e3
Krishna Lagad <krishnalagad2@gmail.com>
2023-08-02 18:32:41 +0530

    - dump starter project.

---
+++ src/main/java/com/learnspring/jwt/JwtRoleAuthApplication.java
@@ -0,0 +1,13 @@
+package com.learnspring.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRoleAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtRoleAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnspring/jwt/JwtRoleAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRoleAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
