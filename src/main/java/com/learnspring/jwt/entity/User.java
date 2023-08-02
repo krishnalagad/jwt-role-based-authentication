ab207684a3b3afeaf016577872c9ff50e1a6605c
Krishna Lagad <krishnalagad2@gmail.com>
2023-08-02 19:04:58 +0530

    - dump join table constraints on user entity.

--- src/main/java/com/learnspring/jwt/entity/User.java
+++ src/main/java/com/learnspring/jwt/entity/User.java
@@ -3,6 +3,8 @@ package com.learnspring.jwt.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Set;
+
 @Entity
 @Table(name = "jwt_roleauth_users")
 @Getter
@@ -28,4 +30,11 @@ public class User {
     private String password;
 
     private String profile;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "jwt_roleauth_users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
 }
